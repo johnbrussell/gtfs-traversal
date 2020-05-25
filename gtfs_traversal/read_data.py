d5ec95d5e8bfa06986b693903cab5e48c724c23d
JBR <john.russell@tufts.edu>
2020-05-25 18:58:21 -0400

    Move load_configuration() to read_data file

--- gtfs_traversal/read_data.py
+++ gtfs_traversal/read_data.py
@@ -1,5 +1,13 @@
+import json
 import os
+
 import gtfs_parsing.analyses.analyses as gtfs_parser
+
+
+def load_configuration():
+    with open("configuration.json") as config_file:
+        config = json.load(config_file)
+    return config
 
 
 def read_data(config, data_folder_name):
