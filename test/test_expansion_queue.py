cc478c7b51ee9115326f30942cf99dbbf327ad63
JBR <john.russell@tufts.edu>
2020-08-09 18:37:08 -0400

    Test ExpansionQueue.add

--- test/test_expansion_queue.py
+++ test/test_expansion_queue.py
@@ -1,6 +1,19 @@
+from gtfs_traversal.data_structures import LocationStatusInfo
+from gtfs_traversal.expansion_queue import ExpansionQueue
 import unittest
 
 
 class TestExpansionQueue(unittest.TestCase):
-    def test_nothing(self):
-        pass
+    def test_add(self):
+        location_a = LocationStatusInfo(location='a', arrival_route=None, unvisited="~~a~~")
+        location_b = LocationStatusInfo(location='b', arrival_route=None, unvisited="~~a~~b~~")
+        nodes = [location_a, location_b]
+        subject = ExpansionQueue(num_solution_stops=2, stop_join_string="~~")
+        subject.add(nodes)
+        expected = {
+            1: [location_a],
+            2: [location_b],
+        }
+        actual = subject._queue
+
+        self.assertDictEqual(expected, actual)
