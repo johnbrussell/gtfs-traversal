7d9a1667932713e329fbc816eda302450b496167
John Russell <john.russell@tufts.edu>
2021-01-21 13:05:12 -0500

    Begin adding infrastructure to calculate minimum travel time from any station to a solution station (#48)
    * Solver -> Traverser
    
    * Traverser extends Solver
    
    * Solver is a variable within Traverser, not a parent class
    
    * Move walk_time_seconds from Traverser to Solver
    
    * NearestStationFinder
    
    * Remove redundant parentheses
    
    * Traverser.reset_time_to_nearest_station
    
    * Traverser.get_time_to_nearest_station
    
    * solver -> traverser in main.py
    
    * Remove unused DataMunger from main
    
    * Consider travel time to nearest station when assessing walking eligibility

---
+++ gtfs_traversal/nearest_station_finder.py
@@ -0,0 +1,7 @@
+class NearestStationFinder:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def travel_time_secs_to_nearest_station():
+        return 0
--- main.py
+++ main.py
@@ -3,9 +3,8 @@ if __name__ == "__main__":
 
     import gtfs_parsing.analyses.analyses as gtfs_analyses
     from gtfs_parsing.data_structures.data_structures import gtfsSchedules, uniqueRouteInfo
-    from gtfs_traversal.data_munger import DataMunger
     from gtfs_traversal.read_data import *
-    from gtfs_traversal.solver import Solver
+    from gtfs_traversal.traverser import Traverser
 
     STOP_JOIN_STRING = '~~'
     TRANSFER_ROUTE = 'transfer'
@@ -49,17 +48,10 @@ if __name__ == "__main__":
 
     data = remove_trips_that_do_not_operate_within_analysis_timeframe(data)
 
-    data_munger = DataMunger(
-        analysis=analysis,
-        data=data,
-        start_time=start_time,
-        stop_join_string=STOP_JOIN_STRING,
-    )
-
-    solver = Solver(analysis=analysis, data=data, progress_between_pruning_progress_dict=10000, prune_thoroughness=.001,
-                    start_time=start_time, stop_join_string=STOP_JOIN_STRING,
-                    transfer_duration_seconds=TRANSFER_DURATION_SECONDS, transfer_route=TRANSFER_ROUTE,
-                    walk_route=WALK_ROUTE, walk_speed_mph=WALK_SPEED_MPH)
+    traverser = Traverser(analysis=analysis, data=data, progress_between_pruning_progress_dict=10000,
+                          prune_thoroughness=.001, start_time=start_time, stop_join_string=STOP_JOIN_STRING,
+                          transfer_duration_seconds=TRANSFER_DURATION_SECONDS, transfer_route=TRANSFER_ROUTE,
+                          walk_route=WALK_ROUTE, walk_speed_mph=WALK_SPEED_MPH)
 
     # end_date_midnight
     best_time = None
@@ -68,7 +60,7 @@ if __name__ == "__main__":
     # start_time = datetime(year=2018, month=10, day=13, hour=22, minute=25)
     while start_time < end_date_midnight:
         print(start_time)
-        new_best_time, new_best_progress_dictionary, earliest_departure_time = solver.find_solution(
+        new_best_time, new_best_progress_dictionary, earliest_departure_time = traverser.find_solution(
             start_time, best_time)
         assert new_best_time is not None
         if best_time is None or new_best_time < best_time:
@@ -83,5 +75,5 @@ if __name__ == "__main__":
 
     print('best start time:', best_start_time)
     print('best time:', best_time)
-    solver.print_path(best_progress_dictionary)
+    traverser.print_path(best_progress_dictionary)
     print("finished successfully.")
