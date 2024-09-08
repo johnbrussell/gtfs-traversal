d8825bcf4caf2311251d9b930e9760c2b780ffa2
JBR <14211416+johnbrussell@users.noreply.github.com>
2024-09-07 23:13:38 -0400

    Move trip_stop_no from progress to location

--- gtfs_traversal_2/data_structures.py
+++ gtfs_traversal_2/data_structures.py
@@ -2,17 +2,25 @@ from collections import namedtuple
 
 
 EarthLocation = namedtuple('EarthLocation', ['lat', 'long'])
-LocationStatusInfo = namedtuple('LocationStatusInfo', ['location', 'arrival_route', 'unvisited', 'num_unvisited'])
+LocationStatusInfo = namedtuple(
+    'LocationStatusInfo',
+    [
+        'location',
+        'arrival_route',
+        'trip_stop_no',
+        'unvisited',
+        'num_unvisited'
+     ],
+)
 ProgressInfo = namedtuple(
     'ProgressInfo',
     [
         'duration',
         'arrival_trip',
-        'trip_stop_no',
         'parent',
         'children',
         'minimum_remaining_time',
         'expanded',
         'eliminated'
-    ]
+    ],
 )
